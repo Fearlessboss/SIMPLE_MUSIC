a90f3d39c2679b095c57f9549fae75f789abf887
L2LUCKY <145443484+L2LUCKY@users.noreply.github.com>
2026-05-12 19:24:12 +0530

    Update config.py

--- config.py
+++ config.py
@@ -41,6 +41,15 @@ ASSUSERNAME = getenv("ASSUSERNAME", "Simple_Boy_1k")
 # ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 
+# ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+# ❖ ᴀ ᴘ ɪ   s ᴇ ᴛ ᴛ ɪ ɴ ɢ s ❖
+# ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+YTPROXY_URL = getenv("YTPROXY_URL", "https://tgapi.xbitcode.com")
+YT_API_KEY = getenv("YT_API_KEY", "") # youtube song api key, generate free key or buy paid plan from http://music.xbitcode.com
+
+API_URL = getenv("API_URL", 'https://api.nexgenbots.xyz') #youtube song url
+VIDEO_API_URL = getenv("VIDEO_API_URL", 'https://api.video.nexgenbots.xyz')
+API_KEY = getenv("API_KEY", "") # youtube song api key, generate free key or buy paid plan from https://console.nexgenbots.xyz
 
 # ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 # ❖ ʟ ɪ ᴍ ɪ ᴛ s   ᴀ ɴ ᴅ   ɪ ᴅ s ❖
