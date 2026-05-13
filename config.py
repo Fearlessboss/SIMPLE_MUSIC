e2502801f80bbee494480a93a477920c8f8b5e0d
L2LUCKY <145443484+L2LUCKY@users.noreply.github.com>
2026-05-13 22:10:58 +0530

    Update config.py

--- config.py
+++ config.py
@@ -46,7 +46,7 @@ MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 # ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 YTPROXY_URL = getenv("YTPROXY_URL", "https://tgapi.xbitcode.com")
 YT_API_KEY = getenv("YT_API_KEY", "") # youtube song api key, generate free key or buy paid plan from http://music.xbitcode.com
-API_URL = getenv("API_URL", 'https://api.nexgenbots.xyz') #youtube song url
+API_URL = getenv("API_URL", "https://api.nexgenbots.xyz") #youtube song url
 VIDEO_API_URL = getenv("VIDEO_API_URL", 'https://api.video.nexgenbots.xyz')
 API_KEY = getenv("API_KEY", "") # youtube song api key, generate free key or buy paid plan from https://console.nexgenbots.xyz
 
